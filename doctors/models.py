1c162dd627e80057f649c27461078ad1cf9e6811
Roberto Barrientos <barrientos.isc@gmail.com>
2026-03-04 00:22:23 -0600

    CRUD de doctors y correcion de modelo en campo address

--- doctors/models.py
+++ doctors/models.py
@@ -6,7 +6,7 @@ class Doctor(models.Model):
     qualification = models.CharField(max_length=100)
     contact_number = models.CharField(max_length=15)
     email = models.EmailField()
-    adress = models.TextField()
+    address = models.TextField()
     biography = models.TextField()
 
 class Department(models.Model):
