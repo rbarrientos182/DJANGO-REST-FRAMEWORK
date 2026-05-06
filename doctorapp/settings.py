f8c37706130485460cc7fd9ae012dfe10e13ccc9
Roberto Barrientos <barrientos.isc@gmail.com>
2026-05-05 19:51:12 -0600

    integraccion de app bookings y docs

--- doctorapp/settings.py
+++ doctorapp/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'patients',
     'doctors',
     'bookings',
+    'docs',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- doctorapp/urls.py
+++ doctorapp/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('patients.urls')),
     path('api/', include('doctors.urls')),
+    #path('api/', include('bookings.urls')),
+    path('', include('docs.urls')),
 ]
